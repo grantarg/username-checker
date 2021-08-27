414872acd5c6036d7d507223efa3b5eb34c0dcc4
grant <trainfantn@live.com>
2021-08-27 14:42:20 -0400

    resized logos and added text fields

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const igBase = 'https://www.instagram.com/'
+const redditBase = 'https://www.reddit.com/user/'
+const steamBase = 'https://steamcommunity.com/id/'
+const twtBase = 'https://twitter.com/'
+const ttvBase = 'https://www.twitch.tv/'
+const pbBase = 'https://pastebin.com/u/'
+const ncBase = 'https://www.namecheap.com/domains/registration/results/?domain=' 
+
+function pullUserRequest () {
+
+}
