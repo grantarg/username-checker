cd0020a84bc78cbd5fb0ac8eca9cde6e19885c36
grant <trainfantn@live.com>
2021-10-12 01:01:13 -0400

    fixed branch issues

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 const igBase = 'https://www.instagram.com/'
 const redditBase = 'https://www.reddit.com/user/'
 const steamBase = 'https://steamcommunity.com/id/'
@@ -6,3 +5,8 @@ const twtBase = 'https://twitter.com/'
 const ttvBase = 'https://www.twitch.tv/'
 const pbBase = 'https://pastebin.com/u/'
 const ncBase = 'https://www.namecheap.com/domains/registration/results/?domain=' 
+
+function pullUserRequest () {
+
+}
+
